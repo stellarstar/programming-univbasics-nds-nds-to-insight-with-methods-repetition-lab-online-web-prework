0a9cf839435b28e0b3d57e6578993c54e81cf61b
victor tim <technicalpro@securemedia.ch>
2020-01-31 12:25:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,7 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+   stooges = [{:name => "Larry"}, {:name => "Curly"}, {:name => "Moe"}, {:name => "Iggy"}]
 end
 
 def total_gross(source)
